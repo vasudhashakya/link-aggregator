795beaaa39804bd32e5d99e002bf8ab9bd85b1d9
vasudhashakya <eeevasudha@yahoo.com>
2017-02-09 12:40:39 -0800

    first quick commit

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.0'
+
+gem 'sqlite3'
+
+gem 'sass-rails', '~> 4.0.0'
+#gem 'sass-rails', '>=3.2'
+gem 'uglifier', '>= 1.3.0'
+
+gem 'coffee-rails', '~> 4.0.0'
+
+gem 'jquery-rails'
+
+gem 'turbolinks'
+
+gem 'jbuilder', '~> 1.2'
+
+gem 'devise', '~> 3.3.0'
+
+#gem 'bootstrap-sass', '~>3.1.1.0'
+gem 'bootstrap-sass', '~>3.2.0.2'
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reddit::Application.config.secret_key_base = 'd1432fa59c840335bdb014d707bf0bbf6c0c481a52207d0cb340c2e91ba19563b1ea8e8668ec9468246db9be6201dc68876acb6813d6942d7e3703e46bbea21a'
